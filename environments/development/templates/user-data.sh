52a4c8025af7d9dba48fda2773908062d89a044a
Miguel Marin <Miguel_Marin@epam.com>
2018-07-17 16:23:48 -0500

    enabling docker log monitoring

--- environments/development/templates/user-data.sh
+++ environments/development/templates/user-data.sh
@@ -5,12 +5,21 @@ echo ECS_CLUSTER=ecs-xport-development >> /etc/ecs/ecs.config
 DD_API_KEY=85f339e1dfe1ddf1b317c167d7994507 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
 usermod -a -G docker dd-agent
 
+sed -i -e 's/# logs_enabled: disabled/logs_enabled: true/g'
+
 cat > /etc/datadog-agent/conf.d/docker.d/docker_daemon.yaml <<EOF
 init_config:
 
 instances:
     - url: "unix://var/run/docker.sock"
       new_tag_names: true
+
+logs:
+    - type: docker
+      service: docker
+      image: 197736343114.dkr.ecr.us-east-1.amazonaws.com/xport
+      source: nginx
+      sourcecategory: web_server
 EOF
 
 stop datadog-agent
